d02f4dff16062f5eee337784a3d1123de04cdaa1
unknown <gksrkfkaryghl@gmail.com>
2023-04-19 23:56:28 +0900

    create popup UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+
+const PopupDiv = styled.div`
+  width: 304px;
+  height: 330px;
+  font-size: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: skyblue;
+  color: white;
+  /* font-weight: bold; */
+  border-radius: 8px;
+  margin: 5px;
+`;
 
 function App() {
+
+  useEffect(() => {
+    // const weatherButton = `<button id="weather-button">날씨</button>`;
+    let weatherButton = document.createElement("button");
+    weatherButton.innerText = "날씨";
+    weatherButton.id = 'weather-button';
+
+    // chrome.storage.local.set({ isWeatherAppRunning : true });
+    // let bodyTag = document.getElementsByTagName("body");
+    // bodyTag.appendChild(weatherButton);
+
+    // console.log("window.opener: ", window.opener);
+    // document.querySelector("body").append(weatherButton);
+
+    // window.opener.document.getElementById("weather-button").style.position = "fixed";
+    // window.opener.document.getElementById("weather-button").style.bottom = "70px";
+    // window.opener.document.getElementById("weather-button").style.left = "50px";
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PopupDiv>
+      Weather Fairy is running!
+    </PopupDiv>
   );
 }
 
